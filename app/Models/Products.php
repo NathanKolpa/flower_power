815024b47f493b7aeba30e6f689f551f9906ac9e
michiellegters123 <michiellegters2@gmail.com>
2020-10-29 15:56:50 +0100

    Product edit

--- app/Models/Products.php
+++ app/Models/Products.php
@@ -22,4 +22,15 @@ class Products extends Model
         'quantity',
     ];
 
+    public function getPriceAttribute($value)
+    {
+        return $value / 100;
+    }
+
+    public function setPriceAttribute($value)
+    {
+        $this->attributes["price"] = $value * 100;
+    }
+
+
 }
